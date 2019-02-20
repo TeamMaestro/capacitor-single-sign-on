61cc6d9d1895fbfd82f241d9a7ff72bbb32e94e4
Sean Bannigan <bannigan@meetmaestro.com>
2019-02-20 12:31:53 -0500

    refactor: remove demo and flatten directories

---
+++ android/@teamhive/capacitor-single-signon/src/main/java/com/teamhive/capacitor/plugin/SingleSignOn.java
@@ -0,0 +1,113 @@
+package com.teamhive.capacitor.plugin;
+
+import android.content.ComponentName;
+import android.content.Intent;
+import android.net.Uri;
+import android.support.customtabs.CustomTabsClient;
+import android.support.customtabs.CustomTabsIntent;
+import android.support.customtabs.CustomTabsServiceConnection;
+import android.support.customtabs.CustomTabsSession;
+import android.util.Log;
+
+import com.getcapacitor.JSObject;
+import com.getcapacitor.NativePlugin;
+import com.getcapacitor.Plugin;
+import com.getcapacitor.PluginCall;
+import com.getcapacitor.PluginMethod;
+import com.getcapacitor.PluginRequestCodes;
+
+@NativePlugin(requestCodes={PluginRequestCodes.BROWSER_OPEN_CHROME_TAB})
+public class SingleSignOn extends Plugin {
+    public static final String CUSTOM_TAB_PACKAGE_NAME = "com.android.chrome";
+
+    private CustomTabsClient customTabsClient;
+    private CustomTabsSession currentSession;
+
+    @PluginMethod()
+    public void authenticate(PluginCall call) {
+        String url = call.getString("url");
+
+        if (url == null) {
+            call.error("Must provide a URL to open");
+            return;
+        }
+        if (url.isEmpty()) {
+            call.error("URL must not be empty");
+            return;
+        }
+
+        saveCall(call);
+        open(url);
+    }
+
+
+    protected void open(String url) {
+        CustomTabsIntent.Builder builder = new CustomTabsIntent.Builder(getCustomTabsSession());
+
+        CustomTabsIntent tabsIntent = builder.build();
+        tabsIntent.intent.putExtra(Intent.EXTRA_REFERRER,
+                Uri.parse(Intent.URI_ANDROID_APP_SCHEME + "//" + getContext().getPackageName()));
+        tabsIntent.launchUrl(getContext(), Uri.parse(url));
+    }
+
+
+    CustomTabsServiceConnection connection = new CustomTabsServiceConnection() {
+        @Override
+        public void onCustomTabsServiceConnected(ComponentName name, CustomTabsClient client) {
+            customTabsClient = client;
+            client.warmup(0);
+        }
+
+        @Override
+        public void onServiceDisconnected(ComponentName name) {
+        }
+    };
+
+    public void load() {
+    }
+
+    protected void handleOnResume() {
+        boolean ok = CustomTabsClient.bindCustomTabsService(getContext(), CUSTOM_TAB_PACKAGE_NAME, connection);
+        if (!ok) {
+            Log.e(getLogTag(), "Error binding to custom tabs service");
+        }
+
+        if (getSavedCall() != null) {
+            getSavedCall().error("Login Unsuccessful");
+            saveCall(null);
+        }
+    }
+
+    protected void handleOnPause() {
+        getContext().unbindService(connection);
+    }
+
+    public CustomTabsSession getCustomTabsSession() {
+        if (customTabsClient == null) {
+            return null;
+        }
+
+        if (currentSession == null) {
+            currentSession = customTabsClient.newSession(null);
+        }
+
+        return currentSession;
+    }
+
+    @Override
+    protected void handleOnNewIntent(Intent intent) {
+        super.handleOnNewIntent(intent);
+        Uri responseUri = intent.getData();
+        if (responseUri != null) {
+            JSObject d = new JSObject();
+            d.put("url", responseUri.toString());
+            getSavedCall().success(d);
+            saveCall(null);
+        }
+    }
+
+    @Override
+    protected void handleOnActivityResult(int requestCode, int resultCode, Intent data) {
+        super.handleOnActivityResult(requestCode, resultCode, data);
+    }
+}
--- demo/src/app/app.component.ts
+++
@@ -1,8 +0,0 @@
-import { Component } from '@angular/core';
-import { Platform } from '@ionic/angular';
-
-@Component({
-    selector: 'app-root',
-    templateUrl: 'app.component.html'
-})
-export class AppComponent {}
